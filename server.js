a3361bb1dfb383f40d2e44b5aba7ea2bd8c2d142
gladson65 <grauth65@gmail.com>
2025-10-17 16:46:56 +0530

    server has been created, database connection code and start with nodemon also done

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// creating server
+const tinyApp = new express();
+tinyApp.listen('7000', ()=> {
+    console.log('server is running ont he port 7000');
+})
+
+// jsonparse middleware
+tinyApp.use(express.json());
+
+// mongoDB database connection
+mongoose.connect('mongodb://localhost:27017/tinyDB');
+const tinyDB = mongoose.connection;
+tinyDB.on('open', ()=> {
+    console.log('Databse connection is successful');
+}) 
+tinyDB.on('error', ()=> {
+    console.log('Something went wrong with datbase')
+})
