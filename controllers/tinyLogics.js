e300ee68f11a0268ad14d61537e5176432c77862
gladson65 <grauth65@gmail.com>
2025-10-18 19:35:36 +0530

    logic section updated

--- controllers/tinyLogics.js
+++ controllers/tinyLogics.js
@@ -14,12 +14,35 @@ export async function shortUrl(req, res) {
         if (url) return res.status(200).json({ shortUrl: `http://localhost:7000/${url.shortcode}` });
 
         // generate short code
-        const shortCode = nanoid(7);
-        url = new tinyModel({ longUrl, shortCode });
+        const shortcode = nanoid(7);
+        url = new tinyModel({ longUrl, shortcode });
         await url.save();
 
-        return res.status(200).json({ shortUrl: `http://localhost:7000/${shortCode}` })
+        return res.status(200).json({ shortUrl: `http://localhost:7000/${shortcode}` })
 
+    }
+    catch(error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+// Redirect short url. Basically get request
+export async function getShortUrl(req, res) {
+
+    // destructuring code from req param
+    const code = req.params.code;
+    console.log(code)
+
+    try {
+        const url = await tinyModel.findOne({ shortcode: code });
+
+        if (!url) return res.status(404).json({message: "url not found"});
+
+        // update the clicks
+        url.clicks++;
+        await url.save();
+
+        res.redirect(url.longUrl);
     }
     catch(error) {
         return res.status(500).json({ error: error.message });
--- routes/tinyRoute.js
+++ routes/tinyRoute.js
@@ -1,6 +1,7 @@
-import { shortUrl } from "../controllers/tinyLogics.js";
+import { shortUrl, getShortUrl } from "../controllers/tinyLogics.js";
 
 export function tinyRoute(tinyApp) {
-    tinyApp.post('/api/shorten', shortUrl)
+    tinyApp.post('/api/shorten', shortUrl),
+    tinyApp.get('/api/:code', getShortUrl)
 }
 
